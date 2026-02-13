4966d95551feaccc00849f7cdd48b2bed5f41283
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2026-02-13 08:11:41 +0100

    complete task 2

---
+++ tasks/task2.py
@@ -0,0 +1,17 @@
+import re
+from typing import Callable, Generator
+
+
+def generator_numbers(text: str) -> Generator[float, None, None]:
+    for match in re.finditer(r"\b\d+\.?\d*\b", text):
+        yield float(match.group())
+
+
+def sum_profit(text: str, func: Callable) -> float:
+    return sum(func(text))
+
+
+if __name__ == "__main__":
+    text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+    total_income = sum_profit(text, generator_numbers)
+    print(f"Загальний дохід: {total_income}")
